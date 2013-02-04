65d38638921d377d9889232399377f61690d0965
= <travcurrit@gmail.com>
2013-02-04 11:07:17 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Algorithmz::Application.config.secret_token = '5b7e493bcba0fd01b1be71978df7840e51d029ccd2968e962f17bc5109e5edca5ea66071253d7f98cbb4f6f167f3f5589d648dcb677460e01e5085e08f38eb23'
